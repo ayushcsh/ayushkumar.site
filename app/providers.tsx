d2c05f39e95602cb472c03bc9e571de663f7b0b2
Ayush Kumar <ayushkumar.nov.2005@gmail.com>
2026-07-04 22:25:21 +0530

    Your commit message

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,12 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
       {children}
     </ThemeProvider>
   );
